b4324de261b63a742d6c2688867e53ea557a79fb
Pranjal Bhardwaj <decimatormind@gmail.com>
2022-03-15 15:51:04 +0530

    Add Static and Dynamic Routes

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
 from flask import Flask
 from flask import render_template
 
-app = Flask("__name__")
+app = Flask(__name__)
 
 @app.route("/")
 def homepage():
-    return render_template("index.html")
+    return render_template('home.html')
+
+@app.route("/about")
+def about():
+    return render_template('about.html')
+
+@app.route("/<int:number>")
+def loop(number):
+    return render_template("loop.html",number = number)
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
 
 if __name__ == "__main__":
     app.run()
